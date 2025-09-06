c4376220523417bedfa1b746acf67e5c4e3da075
ganbin <jlau.ice@gmail.com>
2025-09-06 15:49:48 +0800

    删除样式

--- src/api/work.js
+++ src/api/work.js
@@ -1,6 +1,8 @@
 // 在其他文件中使用
 import request from '../utils/request'
 
-request.get('/users').then((res) => {
-  console.log(res)
-})
+export const getUsers = () => {
+  request.get('/users').then((res) => {
+    console.log(res)
+  })
+}
